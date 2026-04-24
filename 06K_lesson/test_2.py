d387d8c305f85366c794d7f17ae88ff9b5c2d838
Вишневский Михаил Евгеньевич <vishnevskii_m@bezlimit.lan>
2026-04-24 13:39:35 +0300

    исправил мелкие недочеты

--- 06K_lesson/test_2.py
+++ 06K_lesson/test_2.py
@@ -1,4 +1,3 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,27 +5,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 def test_slow_calculator():
-    # 1. Открываем Chrome
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    driver = webdriver.Chrome(service=ChromeService
+                              (ChromeDriverManager().install()))
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
     delay_input = driver.find_element(By.CSS_SELECTOR, "#delay")
     delay_input.clear()
     delay_input.send_keys("45")
-
     driver.find_element(By.XPATH, "//span[text()='7']").click()
     driver.find_element(By.XPATH, "//span[text()='+']").click()
     driver.find_element(By.XPATH, "//span[text()='8']").click()
     driver.find_element(By.XPATH, "//span[text()='=']").click()
-
     waiter = WebDriverWait(driver, 50)
-
     waiter.until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
     )
-
     result = driver.find_element(By.CSS_SELECTOR, ".screen").text
     assert result == "15"
-
     driver.quit()
